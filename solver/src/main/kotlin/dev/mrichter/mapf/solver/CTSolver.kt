ed1bc8fd660c01664695fbc3a28e031c581e16f2
Matyas Richter <matyas@mrichter.cz>
2022-05-17 10:25:26 +0200

    Drop dead code in CTSolver

--- solver/src/main/kotlin/dev/mrichter/mapf/solver/CTSolver.kt
+++ solver/src/main/kotlin/dev/mrichter/mapf/solver/CTSolver.kt
@@ -7,11 +7,13 @@ class ConstraintTreeNode<CT>(
     val parent: ConstraintTreeNode<CT>?,
     val vertexConstraint: VertexConstraint<CT>?,
     val edgeConstraint: EdgeConstraint<CT>?,
-    val agentIndex: Int,
     val agentId: UUID,
     val solution: List<List<CT>>,
     val cost: Int,
 ) {
+    /**
+     * Traverse the tree and collect vertex constraints for an agent.
+     */
     fun getVertexConstraints(id: UUID): Sequence<VertexConstraint<CT>> = sequence {
         if (agentId == id && vertexConstraint != null) {
             yield(vertexConstraint)
@@ -21,6 +23,9 @@ class ConstraintTreeNode<CT>(
         }
     }
 
+    /**
+     * Traverse the tree and collect edge constraints for an agent.
+     */
     fun getEdgeConstraints(id: UUID): Sequence<EdgeConstraint<CT>> = sequence {
         if (agentId == id && edgeConstraint != null) {
             yield(edgeConstraint)
@@ -34,26 +39,12 @@ class ConstraintTreeNode<CT>(
 }
 
 fun <E> Iterable<E>.updated(index: Int, elem: E) = mapIndexed { i, existing -> if (i == index) elem else existing }
-fun <E> Iterable<E>.zipLongest(other: Iterable<E>) = sequence {
-    val a = iterator()
-    val b = other.iterator()
-    while (a.hasNext() && b.hasNext()) {
-        yield(Pair(a.next(), b.next()))
-    }
-    while (a.hasNext()) {
-        yield(Pair(a.next(), other.last()))
-    }
-    while (b.hasNext()) {
-        yield(Pair(last(), b.next()))
-    }
-}
 
 class CTSolver<CT>(
-    val singleAgentSolver: SingleAgentSolver<CT>,
+    private val singleAgentSolver: SingleAgentSolver<CT>,
 ) {
     fun solve(agents: List<Agent<CT>>): Result<ConstraintTreeNode<CT>> {
-        val open =
-            PriorityQueue(compareBy<ConstraintTreeNode<CT>> { it.cost }.thenBy { it.depth })
+        val open = PriorityQueue(compareBy<ConstraintTreeNode<CT>> { it.cost }.thenBy { it.depth })
         val root = createRootNode(agents)
         root.onSuccess { open.add(it) }
         var totalNodes = 0
@@ -67,14 +58,56 @@ class CTSolver<CT>(
         return Result.failure(NotSolvable("Instance is not solvable"))
     }
 
-    private fun makePathPairs(node: ConstraintTreeNode<CT>) = sequence {
-        node.solution.forEachIndexed { indexA, a ->
-            node.solution.withIndex().drop(indexA + 1).forEach { (indexB, b) ->
-                yield(Pair(Pair(indexA, a), Pair(indexB, b)))
-            }
+    private fun createNode(
+        agents: List<Agent<CT>>,
+        parent: ConstraintTreeNode<CT>,
+        index: Int,
+        vertexConstraint: VertexConstraint<CT>?,
+        edgeConstraint: EdgeConstraint<CT>?,
+    ): Result<ConstraintTreeNode<CT>> {
+        return singleAgentSolver.solve(
+            agents[index],
+            0,
+            parent.getVertexConstraints(agents[index].id).toHashSet()
+                .also { set -> vertexConstraint?.let { set.add(it) } },
+            parent.getEdgeConstraints(agents[index].id).toHashSet().also { set -> edgeConstraint?.let { set.add(it) } },
+        ).map {
+            val newSolutionSet = parent.solution.updated(index, it)
+            ConstraintTreeNode(parent,
+                vertexConstraint,
+                edgeConstraint,
+                agents[index].id,
+                newSolutionSet,
+                newSolutionSet.fold(0) { prev, path -> prev + path.size })
         }
     }
 
+    private fun createRootNode(agents: List<Agent<CT>>): Result<ConstraintTreeNode<CT>> {
+        val solution: List<List<CT>>
+        try {
+            val raw = agents.map {
+                singleAgentSolver.solve(it, 0, setOf(), setOf()).getOrThrow()
+            }
+            val solutionLength = raw.maxOf { it.size }
+            solution = raw.map {
+                it + generateSequence { it.last() }.take(solutionLength - it.size)
+            }
+        } catch (e: NotSolvable) {
+            return Result.failure(NotSolvable("Not solvable"))
+        }
+        return Result.success(
+            ConstraintTreeNode(parent = null,
+                vertexConstraint = null,
+                edgeConstraint = null,
+                agentId = agents[0].id,
+                solution = solution,
+                cost = solution.fold(0) { prev, path -> prev + path.size })
+        )
+    }
+
+    /**
+     * Find conflicts by iterating through all paths and create child nodes for them.
+     */
     private fun createChildren(
         node: ConstraintTreeNode<CT>, agents: List<Agent<CT>>
     ): Sequence<ConstraintTreeNode<CT>> {
@@ -89,36 +122,29 @@ class CTSolver<CT>(
             val n1 = mapNextOrLast(iterators)
             for (indexA in n0.indices) {
                 for (indexB in indexA + 1 until node.solution.size) {
+                    // check for vertex conflict
                     if (n0[indexA] == n0[indexB]) {
                         return sequence {
+                            // check if we're not creating a constraint for a finished agent
+                            // because the child node would never reach it
                             if (step < node.solution[indexA].size) {
-                                createNode(
-                                    agents,
-                                    node,
-                                    indexA,
-                                    Pair(n0[indexA], step),
-                                    null,
-                                ).onSuccess {
-                                    yield(it)
-                                }
+                                createNode(agents, node, indexA, Pair(n0[indexA], step), null)
+                                    .onSuccess {
+                                        yield(it)
+                                    }
                             }
                             if (step < node.solution[indexB].size) {
-                                createNode(
-                                    agents,
-                                    node,
-                                    indexB,
-                                    Pair(n0[indexB], step),
-                                    null,
-                                ).onSuccess {
-                                    yield(it)
-                                }
+                                createNode(agents, node, indexB, Pair(n0[indexB], step), null)
+                                    .onSuccess {
+                                        yield(it)
+                                    }
                             }
                         }
                     }
+                    // check for edge conflict
                     if (n0[indexA] == n1[indexB] && n0[indexB] == n1[indexA]) {
                         return sequence {
                             if (step < node.solution[indexA].size) {
-
                                 createNode(
                                     agents,
                                     node,
@@ -148,58 +174,5 @@ class CTSolver<CT>(
             step++
         }
         return emptySequence()
-    }
-
-    private fun createNode(
-        agents: List<Agent<CT>>,
-        parent: ConstraintTreeNode<CT>,
-        index: Int,
-        vertexConstraint: VertexConstraint<CT>?,
-        edgeConstraint: EdgeConstraint<CT>?,
-    ): Result<ConstraintTreeNode<CT>> {
-        return singleAgentSolver.solve(
-            agents[index],
-            0,
-            parent.getVertexConstraints(agents[index].id).toHashSet()
-                .also { set -> vertexConstraint?.let { set.add(it) } },
-            parent.getEdgeConstraints(agents[index].id).toHashSet()
-                .also { set -> edgeConstraint?.let { set.add(it) } },
-        ).map {
-            val newSolutionSet = parent.solution.updated(index, it)
-            ConstraintTreeNode(
-                parent,
-                vertexConstraint,
-                edgeConstraint,
-                index,
-                agents[index].id,
-                newSolutionSet,
-                newSolutionSet.fold(0) { prev, path -> prev + path.size })
-        }
-    }
-
-    private fun createRootNode(agents: List<Agent<CT>>): Result<ConstraintTreeNode<CT>> {
-        val solution: List<List<CT>>
-        try {
-            val raw = agents.map {
-                singleAgentSolver.solve(it, 0, setOf(), setOf()).getOrThrow()
-            }
-            val solutionLength = raw.maxOf { it.size }
-            solution = raw.map {
-                it + generateSequence { it.last() }.take(solutionLength - it.size)
-            }
-        } catch (e: NotSolvable) {
-            return Result.failure(NotSolvable("Not solvable"))
-        }
-        return Result.success(
-            ConstraintTreeNode(
-                parent = null,
-                vertexConstraint = null,
-                edgeConstraint = null,
-                agentIndex = 0,
-                agentId = agents[0].id,
-                solution = solution,
-                cost = solution.fold(0) { prev, path -> prev + path.size }
-            )
-        )
     }
 }
